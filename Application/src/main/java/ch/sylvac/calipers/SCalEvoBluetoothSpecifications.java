821faa544f4eb5565cfd9095862d1a2ab024f257
cesine <cesine@yahoo.com>
2016-01-23 18:00:43 -0500

    updating to Sylvac Metrology profile
    MEM-PM 292-1251-01 - Sylvac's BT profile spec

--- Application/src/main/java/ch/sylvac/calipers/SCalEvoBluetoothSpecifications.java
+++ Application/src/main/java/ch/sylvac/calipers/SCalEvoBluetoothSpecifications.java
@@ -22,22 +22,23 @@ public class SCalEvoBluetoothSpecifications {
     private static HashMap<String, String> attributes = new HashMap();
     public static String SERVICE_A = "00001800-0000-1000-8000-00805f9b34fb";
     public static String SERVICE_B = "00001801-0000-1000-8000-00805f9b34fb";
-    public static String SERVICE_C = "c1b25000-caaf-6d0e-4c33-7dae30052840";
+    public static String METROLOGY = "c1b25000-caaf-6d0e-4c33-7dae30052840";
 
     public static String CHARACTERISTIC_M = "00002a00-0000-1000-8000-00805f9b34fb";
     public static String CHARACTERISTIC_N = "00002a01-0000-1000-8000-00805f9b34fb";
     public static String CHARACTERISTIC_O = "00002a04-0000-1000-8000-00805f9b34fb";
 
-    public static String CHARACTERISTIC_W = "c1b25010-caaf-6d0e-4c33-7dae30052840";
-    public static String CHARACTERISTIC_X = "c1b25011-caaf-6d0e-4c33-7dae30052840";
-    public static String CHARACTERISTIC_Y = "c1b25012-caaf-6d0e-4c33-7dae30052840";
-    public static String CHARACTERISTIC_Z = "c1b25013-caaf-6d0e-4c33-7dae30052840";
+    public static String DATA_RECEIVED = "c1b25010-caaf-6d0e-4c33-7dae30052840";
+    public static String NOT_USED = "c1b25011-caaf-6d0e-4c33-7dae30052840";
+    public static String DATA_REQUEST_OR_COMMAND = "c1b25012-caaf-6d0e-4c33-7dae30052840";
+    public static String ANSWER_TO_REQUEST_OR_COMMAND = "c1b25013-caaf-6d0e-4c33-7dae30052840";
+//01-23 16:25:53.272 777-1803/? D/BtGatt.GattService: onNotify() - address=D1:52:BE:F4:18:2C, charUuid=c1b25013-caaf-6d0e-4c33-7dae30052840, length=8
 
     static {
         // Services
         attributes.put(SERVICE_A, "SERVICE_A");
         attributes.put(SERVICE_B, "SERVICE_B");
-        attributes.put(SERVICE_C, "SERVICE_C");
+        attributes.put(METROLOGY, "METROLOGY");
 
         // SERVICE_A Characteristics
         attributes.put(CHARACTERISTIC_M, "CHARACTERISTIC_M");
@@ -46,11 +47,11 @@ public class SCalEvoBluetoothSpecifications {
 
         // SERVICE_B Characteristics
 
-        // SERVICE_C Characteristics
-        attributes.put(CHARACTERISTIC_W, "CHARACTERISTIC_W");
-        attributes.put(CHARACTERISTIC_X, "CHARACTERISTIC_X");
-        attributes.put(CHARACTERISTIC_Y, "CHARACTERISTIC_Y");
-        attributes.put(CHARACTERISTIC_Z, "CHARACTERISTIC_Z");
+        // METROLOGY Characteristics
+        attributes.put(DATA_RECEIVED, "DATA_RECEIVED");
+        attributes.put(NOT_USED, "NOT_USED");
+        attributes.put(DATA_REQUEST_OR_COMMAND, "DATA_REQUEST_OR_COMMAND");
+        attributes.put(ANSWER_TO_REQUEST_OR_COMMAND, "ANSWER_TO_REQUEST_OR_COMMAND");
     }
 
     public static String lookup(String uuid, String defaultName) {
